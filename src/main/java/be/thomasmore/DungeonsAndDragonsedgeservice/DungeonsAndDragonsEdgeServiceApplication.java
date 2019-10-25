bf7c21929cf490be34630beb255468a4bc0f15f2
maartenschroons <r0761272@student.thomasmore.be>
2019-10-25 09:59:27 +0200

    first commit

---
+++ src/main/java/be/thomasmore/DungeonsAndDragonsedgeservice/DungeonsAndDragonsEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.DungeonsAndDragonsedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonsAndDragonsEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonsAndDragonsEdgeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/thomasmore/DungeonsAndDragonsedgeservice/DungeonsAndDragonsEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.DungeonsAndDragonsedgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonsAndDragonsEdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
