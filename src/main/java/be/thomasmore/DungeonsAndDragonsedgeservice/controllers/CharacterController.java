a09fde3165bb69023daac4ad9e6403b696724031
mnmpower <mnmmaarten@hotmail.com>
2019-11-24 01:07:06 +0100

    complted eddit character

--- src/main/java/be/thomasmore/DungeonsAndDragonsedgeservice/controllers/CharacterController.java
+++ src/main/java/be/thomasmore/DungeonsAndDragonsedgeservice/controllers/CharacterController.java
@@ -1,6 +1,8 @@
 package be.thomasmore.DungeonsAndDragonsedgeservice.controllers;
 
 import be.thomasmore.DungeonsAndDragonsedgeservice.models.Character;
+import be.thomasmore.DungeonsAndDragonsedgeservice.models.GenericResponseWrapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,18 +25,20 @@ public class CharacterController {
     @Autowired
     private RestTemplate restTemplate;
 
-    //    http://localhost:8010/character/name?name=valanthe
-    @RequestMapping("/name")
-    public Character getCharactersByName(@RequestParam("name") String name){
-        Character character = restTemplate.getForObject("http://localhost:8002/characters/search/findCharacterByName?name=" + name, Character.class);
+    //    http://localhost:8010/character/id?id=5
+    @RequestMapping("/id")
+    public Character getCharacterById(@RequestParam("id") Integer id){
+        Character character = restTemplate.getForObject("http://localhost:8002/characters/search/findCharacterById?id=" + id, Character.class);
         return character;
     }
 
-    //    http://localhost:8010/character/id?id=1
-    @RequestMapping("/id")
-    public Character getCharactersById(@RequestParam("id") Integer id){
-        Character character = restTemplate.getForObject("http://localhost:8002/characters/search/findCharacterById?id=" + id, Character.class);
-        return character;
+    //    http://localhost:8010/character/playerid?playerid=1
+    @RequestMapping("/playerid")
+    public List<Character> getCharactersByPlayerid(@RequestParam("playerid") Integer playerid){
+        GenericResponseWrapper wrapper = restTemplate.getForObject("http://localhost:8002/characters/search/findCharactersByPlayeridOrderByName?playerid="+playerid, GenericResponseWrapper.class);
+
+        List<Character> characters = objectMapper.convertValue(wrapper.get_embedded().get("characters"), new TypeReference<List<Character>>() {});
+        return characters;
     }
 
     //    http://localhost:8010/character/create + json character
@@ -44,6 +48,17 @@ public class CharacterController {
         list.add(new MappingJackson2HttpMessageConverter());
         restTemplate.setMessageConverters(list);
         ResponseEntity<String> result = restTemplate.postForEntity("http://localhost:8002/characters/", character, String.class);
+        return  ResponseEntity.ok().build();
+    }
+
+
+    //http://localhost:8010/character/update + json character
+    @PutMapping("/update")
+    public ResponseEntity<String> putCharacterById(@RequestBody Character character){
+        List<HttpMessageConverter<?>> list = new ArrayList<>();
+        list.add(new MappingJackson2HttpMessageConverter());
+        restTemplate.setMessageConverters(list);
+        restTemplate.put("http://localhost:8002/characters/"+character.getId(), character, String.class);
         return  ResponseEntity.ok().build();
     }
 
--- src/main/java/be/thomasmore/DungeonsAndDragonsedgeservice/controllers/RaceController.java
+++ src/main/java/be/thomasmore/DungeonsAndDragonsedgeservice/controllers/RaceController.java
@@ -5,6 +5,7 @@ import be.thomasmore.DungeonsAndDragonsedgeservice.models.Race;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/race")
+@CrossOrigin
 public class RaceController {
 
     @Autowired
